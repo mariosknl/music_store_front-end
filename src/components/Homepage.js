c5fde06da80477469628d40440ceb761c5d6516c
mariosknl <mariosknl@outlook.com>
2020-09-18 18:18:47 +0300

    delete unnecessary code

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Homepage = () => (
-  <>
-    <div className="homepage relative text-center">Welcome to our store!</div>
-    <div className="absolute top-0 m-4 font-bold underline">
-      <Link to="/login" className="text-white">
-        Click to Sign up/Log in
-      </Link>
-    </div>
-  </>
-);
+const Homepage = () => <div className="homepage relative" />;
 export default Homepage;
