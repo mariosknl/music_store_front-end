47e3e66ae7df8f25d002e5c913fd716112c38c6b
mariosknl <mariosknl@outlook.com>
2020-09-29 13:23:14 +0300

    checkUser dependencies removed from app component

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     dispatch(fetchDrumkits());
     dispatch(fetchSnares());
     dispatch(fetchCymbals());
-  }, [dispatch, checkUser]);
+  }, [dispatch]);
 
   return (
     <div className="App">
