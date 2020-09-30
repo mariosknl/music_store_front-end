56628543e1b38dfb64fd0b397e58a0701a8e7684
mariosknl <mariosknl@outlook.com>
2020-09-30 18:23:08 +0300

    updated url for drumkits

--- src/actionCreators/drumkitActions.js
+++ src/actionCreators/drumkitActions.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const fetchDrumkits = createAsyncThunk('instruments/drumkits', async () => {
   const options = {
     method: 'GET',
-    url: 'https://emusicstoreapi.herokuapp.com/v1/instruments/drumkits',
+    url: 'https://emusicstoreapi.herokuapp.com/api/v1/instruments/drumkits',
   };
   const response = await axios(options);
   return response.data;
