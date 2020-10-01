088645347cab9049c5d77da8b92169c5fcd3b062
mariosknl <mariosknl@outlook.com>
2020-10-01 17:04:09 +0300

    animating navbar icon

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -23,6 +23,19 @@ const containerVariants = {
   },
 };
 
+const svgVariants = {
+  hidden: {
+    rotate: -270,
+  },
+  visible: {
+    rotate: 0,
+    transition: {
+      duration: 1,
+      when: 'afterChildren',
+    },
+  },
+};
+
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
@@ -58,18 +71,21 @@ const Navbar = () => {
         animate="visible">
         <div className="navbar-container container">
           <Link to="/" onClick={closeMobileMenu} className="navbar-logo">
-            <svg
+            <motion.svg
               width="80"
               height="45"
               viewBox="0 0 97 95"
               fill="none"
-              xmlns="http://www.w3.org/2000/svg">
+              xmlns="http://www.w3.org/2000/svg"
+              variants={svgVariants}
+              initial="hidden"
+              animate="visible">
               <path
                 id="big_note"
                 d="M24.251 0V63.7279C22.0804 63.0438 19.8127 62.7013 17.5325 62.7133C7.85732 62.7133 0 69.4184 0 78.8937C0 88.369 7.85732 95 17.5325 95C27.2077 95 35.1135 88.0232 35.1135 77.9V33.6452H86.1375V63.7279C83.9669 63.0436 81.6992 62.7012 79.419 62.7133C69.7439 62.7133 61.8865 69.4184 61.8865 78.8937C61.8865 88.369 69.7439 95 79.419 95C89.0942 95 97 88.0232 97 77.9V0H24.251ZM86.1395 23.75H35.1154V10.64H86.1395V23.75Z"
                 fill="#CED4DA"
               />
-            </svg>
+            </motion.svg>
           </Link>
         </div>
         <div className="h-4 w-1/6 xl:text-3xl font-bold md:text-2xl sm:text-base text-white">
